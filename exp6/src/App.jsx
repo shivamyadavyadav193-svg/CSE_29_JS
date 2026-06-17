ff04b97715fca0e8875feedfc027d689babb52c2
SHIVAM KUMAR 540 <shivamyadavyadav193@gmail.com>
2026-06-17 15:34:52 +0530

    update 4 ,5,6

---
+++ exp6/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./components/Form";
+import ApiData from "./components/ApiData";
+
+function App() {
+  return (
+    <>
+      <Form />
+      <ApiData />
+    </>
+  );
+}
+
+export default App;
